5a7088ff7b1683abab4692de2f6cf605e9a95b52
jcuddles@msn.com <jcuddles@msn.com>
2018-04-04 13:39:09 +0100

    Adding Button and Functionality

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -15,9 +18,16 @@ public class Main extends Application {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Group root = new Group();
         Text txt = new Text("Well Boy!");
+        TextField nameFld = new TextField();
+        Button btn = new Button();
+        btn.setText("Say Well Boy!");
+        btn.setOnAction(evt -> System.out.printf("Well Boy %s %n",
+                nameFld. getText()));
         primaryStage.setTitle("WellBoy");
-        root.getChildren().add(txt);
         txt.setY(10);
+        VBox box = new VBox();
+        box.getChildren().addAll(txt,nameFld, btn);
+        root.getChildren().add(box);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
