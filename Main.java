f7611ac970994febdcae3ab9e7e0f2d5654bfe89
jcuddles@msn.com <jcuddles@msn.com>
2018-04-04 13:15:48 +0100

    Adding text

--- Main.java
+++ Main.java
@@ -2,16 +2,22 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root = new Group();
+        Text txt = new Text("Well Boy!");
+        primaryStage.setTitle("WellBoy");
+        root.getChildren().add(txt);
+        txt.setY(10);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
