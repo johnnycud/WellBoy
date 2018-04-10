4ab1b8cd380d234d94520188dcf06ae4d7e9a210
jcuddles@msn.com <jcuddles@msn.com>
2018-04-10 15:29:17 +0100

    Using Controller

--- Controller.java
+++ Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField firstName;
+
+    public void handleSayWellBoy(ActionEvent actionEvent) {
+        System.out.printf("Well Boy %s! %n", firstName.getText());
+    }
 }
